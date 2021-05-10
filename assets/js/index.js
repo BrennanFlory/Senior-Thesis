707ad91f1f422361972015d95546c76e038d9c5f
Brennan J Flory <brennanjflory@gmail.com>
2021-05-10 01:49:36 -0400

    fix 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,24 @@
-/*
-Copyright 2020 Adobe
-All Rights Reserved.
-
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it. If you have received this file from a source other than Adobe,
-then your use, modification, or distribution of it requires the prior
-written permission of Adobe.
-*/
-
-/* Pass the embed mode option here */
 const viewerConfig = {
     embedMode: "LIGHT_BOX"
 };
 
-/* Wait for Adobe Document Services PDF Embed API to be ready and enable the View PDF button */
 document.addEventListener("adobe_dc_view_sdk.ready", function() {
     document.getElementById("view-pdf-btn").disabled = false;
 });
 
-/* Function to render the file using PDF Embed API. */
-function previewFile() {
-    /* Initialize the AdobeDC View object */
+function preview117() {
     var adobeDCView = new AdobeDC.View({
-        /* Pass your registered client id */
         clientId: "33f2379e58524e4fbbd8093b6c2b6ceb"
     });
 
-    /* Invoke the file preview API on Adobe DC View object */
     adobeDCView.previewFile({
-        /* Pass information on how to access the file */
         content: {
-            /* Location of file where it is hosted */
             location: {
-                url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf",
-                /*
-                If the file URL requires some additional headers, then it can be passed as follows:-
-                header: [
-                    {
-                        key: "<HEADER_KEY>",
-                        value: "<HEADER_VALUE>",
-                    }
-                ]
-                */
+                url: "../../EMR/html/117/EMR2020_117.pdf",
             },
         },
-        /* Pass meta data of file */
         metaData: {
-            /* file name */
-            fileName: "Bodea Brochure.pdf"
+            fileName: "EMR2020_117"
         }
     }, viewerConfig);
 };
