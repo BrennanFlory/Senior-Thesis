51b751ebe6c7408d4578acfa820ce351007a2fe0
Brennan J Flory <brennanjflory@gmail.com>
2021-05-19 21:44:35 -0400

    finsihish

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,34 +1,5 @@
-function search() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                td = tr[i].getElementsByTagName("td")[1];
-                if (td) {
-                    txtValue = td.textContent || td.innerText;
-                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = "";
-                    } else {
-                        td = tr[i].getElementsByTagName("td")[2];
-                        if (td) {
-                            txtValue = td.textContent || td.innerText;
-                            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                                tr[i].style.display = "";
-                            } else {
-                                tr[i].style.display = "none";
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+const searchInstance = new mdb.Datatable.getInstance(document.getElementById('datatable'))
+
+document.getElementById('datatable-search-input').addEventListener('input', (e) => {
+    searchInstance.search(e.target.value);
+});
